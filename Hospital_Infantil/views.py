02bef55a819451599f30659cd61fcc5063e866a0
Arcemontoya <a1271264@uabc.edu.mx>
2024-10-13 20:09:34 -0700

    Pantalla de login con pantalla, nuevo logo

--- Hospital_Infantil/views.py
+++ Hospital_Infantil/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(render(request, "index.html"))
+    return HttpResponse(render(request, "login.html"))
