d5bd902596a73fec10cbf490ec147ea390f1981c
Arcemontoya <a1271264@uabc.edu.mx>
2024-10-18 23:17:10 -0700

    Primera versión de pantalla de asignación de tratamiento (médico)

--- Hospital_Infantil/settings.py
+++ Hospital_Infantil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5t#ndq279(iil5d5%37_&ih92^kp*-wxg8*7a&aetf53a6i(c^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.72", "localhost"]
+ALLOWED_HOSTS = ["192.168.1.72", "localhost", "10.32.0.109"]
 
 
 # Application definition
