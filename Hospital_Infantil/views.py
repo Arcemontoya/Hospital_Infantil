cb6a4923820ae36ed5ad9be375733a0388aadbeb
Arcemontoya <a1271264@uabc.edu.mx>
2024-10-19 10:38:33 -0700

    Agregado perfil paciente e imagenes de interfaz 256x256

--- Hospital_Infantil/views.py
+++ Hospital_Infantil/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(render(request, "medicoTratamiento.html"))
+    return HttpResponse(render(request, "perfilPaciente.html"))
